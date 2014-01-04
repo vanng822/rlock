1cfba99412470557e58e2d2342eac770a507e1bb
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-01-04 21:22:32 +0100

    Checking the value in redis as well

--- tests/test_expired_lock.js
+++ tests/test_expired_lock.js
@@ -5,7 +5,7 @@ var redis = require('redis');
 var testUtil = require('./util.js');
 
 vows.describe('Test suite for expired lock').addBatch({
-	'acquire lock expire1' : {
+	'acquire expired lock expire1' : {
 		'topic' : function() {
 			var self = this;
 			testUtil.setRedisKey('rlock::expire1', Date.now() - 1000000, function(err, result) {
@@ -19,13 +19,21 @@ vows.describe('Test suite for expired lock').addBatch({
 		},
 		'should be ok' : function(err, done) {
 			assert.ok(done !== null);
-			testUtil.deleteRedisKey('rlock::expire1');
 		},
 		'and lock flag is true' : function(err, done) {
 			assert.ok(this.lock._locked);
 		},
 		'and no need of retry': function(err, done) {
 			assert.ok(this.lock.retries === 0);
+		},
+		'and when getting value from redis': {
+			'topic': function() {
+				testUtil.getRedisKey('rlock::expire1', this.callback);
+			},
+			'it should have same value' : function(err, result) {
+				assert.ok(this.lock._expire === parseInt(result));
+				testUtil.deleteRedisKey('rlock::expire1');
+			}
 		}
 	}
 }).export(module);
